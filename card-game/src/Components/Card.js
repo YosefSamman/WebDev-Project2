a11555193ddb15ca547310ba5ab5126e59ae1ed4
Yosef Samman <ID+yosef.samman5@users.noreply.github.com>
2023-03-29 16:26:41 -0400

    updated Card.js file to give an alert when game is completed

--- card-game/src/Components/Card.js
+++ card-game/src/Components/Card.js
@@ -1,1 +1,7 @@
 import './Card.css';
+
+export default function Card(card, completed, flipped, disabled, handler) {
+    if(completed() === true){
+        window.alert("You have completed");
+    }
+}
